84d0b7041a98b371bc5391578635c980361b9c98
Derek Beauregard <dbeauregard@gmail.com>
2024-12-18 14:29:54 -0700

    change to supported (exclusive, non-durable) queue type

--- src/main/java/io/dbeauregard/rabbit/RabbitApplication.java
+++ src/main/java/io/dbeauregard/rabbit/RabbitApplication.java
@@ -1,7 +1,5 @@
 package io.dbeauregard.rabbit;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
@@ -9,7 +7,6 @@ import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +14,6 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class RabbitApplication {
 
-	private static Logger log = LoggerFactory.getLogger(RabbitApplication.class);
 	static final String topicExchangeName = "spring-boot-exchange";
 	static final String queueName = "spring-boot";
 
@@ -26,15 +22,8 @@ public class RabbitApplication {
 	}
 
 	@Bean
-	public CommandLineRunner test() {
-		return (args) -> {
-			log.info("Hello Rabbit");
-		};
-	}
-
-	@Bean
 	Queue queue() {
-		return new Queue(queueName, false);
+		return new Queue(queueName, false, true, false);
 	}
 
 	@Bean
