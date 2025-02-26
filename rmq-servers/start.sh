ad6e46ee342d57ba64aab5faa7bfc86217a62179
Derek Beauregard <dbeauregard@gmail.com>
2025-02-25 17:53:25 -0700

    update start script to support detached startup

--- rmq-servers/start.sh
+++ rmq-servers/start.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <Server Number>"
+if [[ $# -ne 1 && $# -ne 2 ]]; then
+    echo "Usage: $0 <Server Number> [-detached]"
     exit 1
 fi
 
+if [[ $# -eq 2 ]]; then
+    if [[ $2 == "-detached" ]]; then
+        echo "Running detached"
+    else
+        echo "Usage: $0 <Server Number> [-detached]"
+        exit 1
+    fi
+fi
+
 echo "Starting RabbitMQ Server# $1"
+
 export RABBITMQ_CONF_ENV_FILE="/Users/dbeauregard/Workspace/rabbit/rmq-servers/rabbitmq$1-env.conf"
 echo "Config File: $RABBITMQ_CONF_ENV_FILE"
 
-/opt/homebrew/opt/rabbitmq/sbin/rabbitmq-server
+/opt/homebrew/opt/rabbitmq/sbin/rabbitmq-server $2
