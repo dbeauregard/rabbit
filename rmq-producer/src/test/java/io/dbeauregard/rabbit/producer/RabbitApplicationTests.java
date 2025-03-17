74f9a6fcb4a4d9d7a538d48c9975fe8c64b1a7e3
Derek Beauregard <dbeauregard@gmail.com>
2025-03-17 17:43:47 -0600

    enable testcontainers for RMQ

--- rmq-producer/src/test/java/io/dbeauregard/rabbit/producer/RabbitApplicationTests.java
+++ rmq-producer/src/test/java/io/dbeauregard/rabbit/producer/RabbitApplicationTests.java
@@ -2,9 +2,18 @@ package io.dbeauregard.rabbit.producer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class RabbitApplicationTests {
+
+	@Container
+	@ServiceConnection
+	private static final RabbitMQContainer rabbitMQContainer = new RabbitMQContainer("rabbitmq");
 
 	@Test
 	void contextLoads() {
